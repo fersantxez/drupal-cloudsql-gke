5b0dcf37ae7cd3e3701c45a6d9b50fe5a6486c37
Fernando Sanchez <fernandosanchezmunoz@gmail.com>
2017-10-13 23:25:02 -0400

    Update cloudsql.sh

--- scripts/cloudsql.sh
+++ scripts/cloudsql.sh
@@ -2,16 +2,6 @@
 
 source ./env.sh
 
-#gcloud sql instances create INSTANCE [--activation-policy=ACTIVATION_POLICY] [--assign-ip] [--async] [--authorized-gae-apps=APP,[APP,…]]
-# [--authorized-networks=NETWORK,[NETWORK,…]] [--no-backup] [--backup-start-time=BACKUP_START_TIME] [--cpu=CPU] [--database-flags=FLAG=VALUE,[FLAG=VALUE,…]]
-# [--database-version=DATABASE_VERSION; default="MYSQL_5_6"] [--enable-bin-log] [--failover-replica-name=FAILOVER_REPLICA_NAME]
-# [--follow-gae-app=FOLLOW_GAE_APP] 
-# [--gce-zone=GCE_ZONE] [--maintenance-release-channel=MAINTENANCE_RELEASE_CHANNEL]
-# [--maintenance-window-day=MAINTENANCE_WINDOW_DAY] [--maintenance-window-hour=MAINTENANCE_WINDOW_HOUR]
-# [--master-instance-name=MASTER_INSTANCE_NAME] [--memory=MEMORY] [--pricing-plan=PRICING_PLAN, -p PRICING_PLAN; default="PER_USE"] 
-# [--region=REGION; default="us-central"] [--replica-type=REPLICA_TYPE] [--replication=REPLICATION] [--require-ssl] 
-# [--storage-auto-increase] [--storage-size=STORAGE_SIZE] [--storage-type=STORAGE_TYPE] [--tier=TIER, -t TIER] [GCLOUD_WIDE_FLAG …]
-
 gcloud sql instances create $CLOUDSQL_INSTANCE \
 --region=$REGION \
 --gce-zone=$ZONE \
