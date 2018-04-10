ed7f514dbefb892a142ec0d15b4721620de23363
Fernando Sanchez <fersanchez@rodete-desktop-gce-imager2.corp.google.com>
2018-04-09 20:32:11 -0400

    vm plus firewall rule

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -12,6 +12,7 @@ resource "google_compute_instance" "default" {
  zone = "${data.google_compute_zones.available.names[0]}"
  name = "tf-compute-1"
  machine_type = "f1-micro"
+ tags = ["web"]
  boot_disk {
    initialize_params {
      image = "ubuntu-1604-xenial-v20170328"
@@ -22,6 +23,27 @@ resource "google_compute_instance" "default" {
    access_config {
    }
  }
+ metadata_startup_script = <<-EOF
+                          #!/bin/bash
+                          echo "Hello, Mundo" > index.html
+                          nohup busybox httpd -f -p 8080 &
+                          EOF
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = "default"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "1000-2000"]
+  }
+
+  target_tags = ["web"]
 }
 
 output "instance_id" {
