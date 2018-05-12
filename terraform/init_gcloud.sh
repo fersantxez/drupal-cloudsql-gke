7a0b5094ae5ce24cedb1f407da6a3d30ea63d1b7
Fernando Sanchez <fersanchez@rodete-desktop-gce-imager2.corp.google.com>
2018-05-11 22:41:37 -0400

    check whether apis and roles are enabled before enabling them

--- terraform/init_gcloud.sh
+++ terraform/init_gcloud.sh
@@ -26,37 +26,73 @@ command -v gcloud >/dev/null 2>&1 || { echo "I require gcloud but it's not insta
 
 #login to gcloud and set project params
 echo "**INFO: logging into gcloud and setting up the project"
-gcloud auth login --no-launch-browser && \
-#gcloud projects create \
-#  ${TF_VAR_project} --name=${TF_VAR_project} --organization=${TF_VAR_org_id} \
-#  --enable-cloud-apis --set-as-default && \
-gcloud config set account ${ACCOUNT_ID} && \
-gcloud config set project ${TF_VAR_project} && \
-gcloud config set compute/zone ${TF_VAR_zone}
-
-echo "**INFO: enabling APIs on project"
-gcloud services enable \
-compute.googleapis.com \
-container.googleapis.com \
-dns.googleapis.com \
-iam.googleapis.com \
-replicapool.googleapis.com \
-replicapoolupdater.googleapis.com \
-resourceviews.googleapis.com \
-sql-component.googleapis.com \
-sqladmin.googleapis.com \
-storage-api.googleapis.com \
-storage-component.googleapis.com \
-cloudresourcemanager.googleapis.com
+export LOGGED_ACCOUNT=$(gcloud config list account | awk '{print $3}' | sed -n 2,3p)
+echo "**DEBUG: logged account is ["${LOGGED_ACCOUNT}"]"
+echo "**DEBUG: account ID is ["${ACCOUNT_ID}"]"
+if [[ "$LOGGED_ACCOUNT" == "$ACCOUNT_ID" ]]; then
+    echo "**INFO: Logged in as "$LOGGED_ACCOUNT
+else
+    echo "**INFO: Not logged in. Logging in as "${ACCOUNT_ID}
+    gcloud auth login --brief --no-launch-browser && \
+    #gcloud projects create \
+    #  ${TF_VAR_project} --name=${TF_VAR_project} --organization=${TF_VAR_org_id} \
+    #  --enable-cloud-apis --set-as-default && \
+    gcloud config set account ${ACCOUNT_ID} && \
+    gcloud config set project ${TF_VAR_project} && \
+    gcloud config set compute/zone ${TF_VAR_zone}
+fi
 
 # make sure that the relevant APIs are enabled
-echo "**INFO: finding project ID for project "${TF_VAR_project}
-export PROJECT_ID=$(gcloud compute project-info describe \
-                |grep 'id:' \
-                |awk '{print $2}')
-echo "**Project ID found: "${PROJECT_ID}
+echo "**INFO: enabling APIs on project"
 
-#make sure service account in the variables exists
+export ENABLED_APIS=$(gcloud services list --enabled | awk '{print $1}' | tail -n +1)
+
+for ea in $ENABLED_APIS; do
+    echo "**DEBUG: ENABLED API ["$ea"]"
+done
+
+declare -a REQUIRED_APIS=(\
+    "container.googleapis.com" \
+    "compute.googleapis.com" \
+    "dns.googleapis.com" \
+    'iam.googleapis.com' \
+    'replicapool.googleapis.com' \
+    'replicapoolupdater.googleapis.com' \
+    'resourceviews.googleapis.com' \
+    'sql-component.googleapis.com' \
+    'sqladmin.googleapis.com' \
+    'storage-api.googleapis.com' \
+    'storage-component.googleapis.com' \
+    'cloudresourcemanager.googleapis.com' \
+ )
+for ra in "${REQUIRED_APIS[@]}"; do
+    echo "**DEBUG: REQUIRED API ["$ra"]"
+done
+
+function contains() {
+    local n=$#
+    local value=${!n}
+    for ((i=1;i < $#;i++)) {
+        if [ "${!i}" == "${value}" ]; then
+            echo "y"
+            return 0
+        fi
+    }
+    echo "n"
+    return 1
+}
+
+for api in "${REQUIRED_APIS[@]}"; do
+    echo "**DEBUG: API "$api" is required"
+    if [[ " ${ENABLED_APIS[@]} " =~ "${api}" ]]; then
+        echo "**INFO: API "$api" is enabled."
+    else
+        echo "**INFO: API "$api" is disabled. Enabling it..."
+        gcloud services enable $api
+    fi
+done
+
+#make sure Service Account exists
 echo "**INFO: validating Service Accounts"
 export SERVICE_ACCOUNT_LIST=$(gcloud iam service-accounts list  \
     | tail -n +2 | awk '{print $1}')            #get first column. SA description does not have spaces.
@@ -67,74 +103,56 @@ for i in ${SERVICE_ACCOUNT_LIST};do
     if [ $i = "${ADMIN_SVC_ACCOUNT}" ] ; then
         export SA_FOUND=true
         echo "Service Account "$i" found"
-        echo "Please make sure it has the right permissions, or delete it manually and re-create it using this script."
         break
     fi
 done
 
 if [ "${SA_FOUND}" = false ]; then
     echo "**ERROR: Service account "${ADMIN_SVC_ACCOUNT}" not found in project "${TF_VAR_project}
-    echo "**INFO: Do you want me to create it and enable the required permissions?"
-    while true; do
-    read -p "** Enter (y/n): " RESPONSE
+    echo "**ERROR: Do you want me to create it?"
+    read -p "** (y/n): " RESPONSE
     case $RESPONSE in
-        [yY]) echo "***INFO: Creating service account "${ADMIN_SVC_ACCOUNT}" on project "${TF_VAR_project}
+        [yY]) echo "**INFO: Creating service account "${ADMIN_SVC_ACCOUNT}" on project "${TF_VAR_project}
             #create service account
             gcloud iam service-accounts create ${ADMIN_SVC_ACCOUNT} \
-                --display-name ${ADMIN_SVC_ACCOUNT}  \
-            #add relevant permissions
-            gcloud projects add-iam-policy-binding ${TF_VAR_project} \
-                --member serviceAccount:${ADMIN_SVC_ACCOUNT}@${TF_VAR_project}.iam.gserviceaccount.com \
-                --role 'roles/iam.roleAdmin'  
-            gcloud projects add-iam-policy-binding ${TF_VAR_project} \
-                --member serviceAccount:${ADMIN_SVC_ACCOUNT}@${TF_VAR_project}.iam.gserviceaccount.com \
-                --role 'roles/iam.serviceAccountAdmin'
-            gcloud projects add-iam-policy-binding ${TF_VAR_project} \
-                --member serviceAccount:${ADMIN_SVC_ACCOUNT}@${TF_VAR_project}.iam.gserviceaccount.com \
-                --role 'roles/iam.serviceAccountActor'
-            gcloud projects add-iam-policy-binding ${TF_VAR_project} \
-                --member serviceAccount:${ADMIN_SVC_ACCOUNT}@${TF_VAR_project}.iam.gserviceaccount.com \
-                --role 'roles/iam.serviceAccountKeyAdmin'
-            gcloud projects add-iam-policy-binding ${TF_VAR_project} \
-                --member serviceAccount:${ADMIN_SVC_ACCOUNT}@${TF_VAR_project}.iam.gserviceaccount.com \
-                --role 'roles/resourcemanager.projectIamAdmin'       
-            gcloud projects add-iam-policy-binding ${TF_VAR_project} \
-                --member serviceAccount:${ADMIN_SVC_ACCOUNT}@${TF_VAR_project}.iam.gserviceaccount.com \
-                --role 'roles/compute.storageAdmin'
-            gcloud projects add-iam-policy-binding ${TF_VAR_project} \
-                --member serviceAccount:${ADMIN_SVC_ACCOUNT}@${TF_VAR_project}.iam.gserviceaccount.com \
-                --role 'roles/storage.admin'
-            gcloud projects add-iam-policy-binding ${TF_VAR_project} \
-                --member serviceAccount:${ADMIN_SVC_ACCOUNT}@${TF_VAR_project}.iam.gserviceaccount.com \
-                --role 'roles/storage.objectAdmin'  
-            gcloud projects add-iam-policy-binding ${TF_VAR_project} \
-                --member serviceAccount:${ADMIN_SVC_ACCOUNT}@${TF_VAR_project}.iam.gserviceaccount.com \
-                --role 'roles/compute.securityAdmin'  
-            gcloud projects add-iam-policy-binding ${TF_VAR_project} \
-                --member serviceAccount:${ADMIN_SVC_ACCOUNT}@${TF_VAR_project}.iam.gserviceaccount.com \
-                --role 'roles/compute.networkAdmin'  
-            gcloud projects add-iam-policy-binding ${TF_VAR_project} \
-                --member serviceAccount:${ADMIN_SVC_ACCOUNT}@${TF_VAR_project}.iam.gserviceaccount.com \
-                --role 'roles/compute.instanceAdmin.v1'  
-            gcloud projects add-iam-policy-binding ${TF_VAR_project} \
-                --member serviceAccount:${ADMIN_SVC_ACCOUNT}@${TF_VAR_project}.iam.gserviceaccount.com \
-                --role 'roles/cloudsql.admin'
-            gcloud projects add-iam-policy-binding ${TF_VAR_project} \
-                --member serviceAccount:${ADMIN_SVC_ACCOUNT}@${TF_VAR_project}.iam.gserviceaccount.com \
-                --role 'roles/container.admin'
-            gcloud projects add-iam-policy-binding ${TF_VAR_project} \
-                --member serviceAccount:${ADMIN_SVC_ACCOUNT}@${TF_VAR_project}.iam.gserviceaccount.com \
-                --role 'roles/dns.admin'
+                --display-name ${ADMIN_SVC_ACCOUNT}
             break                                                                            
             ;;
-        [nN]) echo "***ERROR: Please create the Service Account and assign IAM roles manually or re-run this script. Exiting. "
+        [nN]) echo "**ERROR: Terraform Service account is required. Exiting. "
             exit
             ;;
         *) echo "**ERROR: Invalid input. Please select [y] or [n]"
             ;;
     esac
-    done
 fi
+
+#Ensure Service Account has required permissions
+echo "**INFO: Enabling Service Account roles"
+
+declare -a SA_REQUIRED_ROLES=(\
+    "roles/iam.roleAdmin" \
+    "roles/iam.serviceAccountAdmin" \
+    "roles/iam.serviceAccountActor" \
+    "roles/iam.serviceAccountKeyAdmin" \
+    "roles/resourcemanager.projectIamAdmin" \
+    "roles/compute.storageAdmin" \
+    "roles/storage.admin" \
+    "roles/storage.objectAdmin" \
+    "roles/compute.securityAdmin" \
+    "roles/compute.networkAdmin" \
+    "roles/compute.instanceAdmin.v1" \
+    "roles/cloudsql.admin" \
+    "roles/container.admin" \
+    "roles/dns.admin" \
+    )
+
+for role in ${SA_REQUIRED_ROLES[@]}; do
+    echo "**INFO: enabling role "$role
+    gcloud projects add-iam-policy-binding ${TF_VAR_project} \
+        --member serviceAccount:${ADMIN_SVC_ACCOUNT}@${TF_VAR_project}.iam.gserviceaccount.com \
+        --role $role \
+        > /dev/null 2>&1
+done
 
 #download the service account credentials to the right location
 echo "**INFO: creating Service Account keys"
@@ -144,9 +162,9 @@ else
     echo "**INFO: No Service Account keys found. Creating them as "${TF_VAR_CREDS}
     gcloud iam service-accounts keys create \
         ${TF_VAR_CREDS} \
-        --iam-account ${ADMIN_SVC_ACCOUNT}@${TF_VAR_project}.iam.gserviceaccount.com && \
-    export GOOGLE_DEFAULT_CREDENTIALS=${TF_VAR_CREDS}
+        --iam-account ${ADMIN_SVC_ACCOUNT}@${TF_VAR_project}.iam.gserviceaccount.com 
 fi
+export GOOGLE_DEFAULT_CREDENTIALS=${TF_VAR_CREDS}
 
 #make bucket - catch if it already exists so we don't exit but ask
 echo "**INFO: creating bucket for Terraform state"
@@ -196,8 +214,3 @@ echo "**INFO: Drupal will be available at the lb_ip address above" && \
 exit
 
 echo "**ERROR: please run 'terraform apply' again"
-
-
-
-
-
