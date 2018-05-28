14783886b107a5ef47f05b1ba027e846fa2dd668
fersanchez@google.comgit config --global user.name fersanchez@google.comgit config --global user.name fersanchez@google.com <fersanchez@fersanchez-macbookpro.roam.corp.google.com>
2018-05-28 07:03:20 -0400

    change to docker variable setting

--- terraform/run.sh
+++ terraform/run.sh
@@ -3,6 +3,9 @@ set -o errexit -o nounset -o pipefail
 
 source ./env.sh
 
+#check environment
+##################
+
 #make sure there is an internet connection
 if ping -q -c 1 -W 1 google.com >/dev/null; then
   echo "**INFO: Internet connectivity is working."
@@ -24,7 +27,39 @@ fi
 echo "**INFO: Validating environment"
 command -v gcloud >/dev/null 2>&1 || { echo "I require gcloud but it's not installed.  Aborting." >&2; exit 1; }
 
+#check required variables are defined - need to be passed on from Dockerfile
+#if [ -z "$var" ]; then echo "var is unset"; else echo "var is set to '$var'"; fi
+export VARS="ACCOUNT_ID ORG_ID BILLING_ACCOUNT PROJECT REGION ZONE MASTER_PASSWORD"
+for var in $VARS; do
+  if [ -z "$var" ]; then echo $var" is unset, exiting" && exit; else echo $var" is set to '$var'"; fi
+done
+
+#check MASTER_PASSWORD is at least 20 chars long
+if [[ ${#MASTER_PASSWORD} -le 19 ]]; then
+    echo "**ERROR: MASTER PASSWORD must be AT LEAST 20 characters long"
+    exit
+else
+    echo "**INFO: MASTER PASSWORD saved, "${#MASTER_PASSWORD}" characters long."
+fi
+
+#create master password
+#not required as MASTER_PASSWORD now needs to be passed as var from Dockerfile
+'
+while true; do
+    echo "**INFO: PLEASE ENTER ***MASTER PASSWORD*** (needs to be AT LEAST 20 characters long)" 
+    read -s TF_VAR_master_password
+    if [[ ${#TF_VAR_master_password} -le 19 ]]; then
+        echo "**ERROR: MASTER PASSWORD must be AT LEAST 20 characters long"
+    else
+        echo "**INFO: MASTER PASSWORD saved, "${#TF_VAR_master_password}" characters long."
+        break
+    fi
+done
+'
+
 #login to gcloud and set project params
+#######################################
+
 echo "**INFO: Logging into gcloud and setting up the project"
 export LOGGED_ACCOUNT=$(gcloud config list account | awk '{print $3}' | sed -n 2,3p)
 export LOGGED_PROJECT=$(gcloud config list|grep project|awk '{print $3}')
@@ -40,6 +75,8 @@ echo "**INFO: Logged in as "$LOGGED_ACCOUNT
 echo "**INFO: Logged onto project "${TF_VAR_project}
 
 # make sure that the relevant APIs are enabled
+##############################################
+
 echo "**INFO: Enabling APIs on project"
 export ENABLED_APIS=$(gcloud services list --enabled | awk '{print $1}' | tail -n +1)
 #REQUIRED_APIS defined in env.sh as array
@@ -52,6 +89,9 @@ for api in "${REQUIRED_APIS[@]}"; do
         gcloud services enable $api
     fi
 done
+
+# create and enable Terraform Service Account
+#############################################
 
 #make sure Service Account exists
 echo "**INFO: Validating Service Accounts"
@@ -102,6 +142,12 @@ else
         --iam-account ${ADMIN_SVC_ACCOUNT}@${TF_VAR_project}.iam.gserviceaccount.com 
 fi
 
+# create bucket for Terraform state
+###################################
+
+#remove previous state
+rm -Rf .terraform/
+
 #make bucket - catch if it already exists so we don't exit but ask
 echo "**INFO: Creating bucket for Terraform state"
 if gsutil ls gs://${TF_VAR_bucket_name}; then
@@ -121,27 +167,15 @@ else
 fi
 
 #update backend template file
+#############################
 echo "**INFO: Updating backend from template"
 rm -f backend.tf
 cp backend.tf.template backend.tf
 sed -i `` "s,__BUCKET__,$TF_VAR_bucket_name,g" backend.tf
 sed -i `` "s,__PROJECT__,$TF_VAR_project,g" backend.tf
 
-#remove previous state
-rm -Rf .terraform/
-
-#create master password
-while true; do
-    echo "**INFO: PLEASE ENTER ***MASTER PASSWORD*** (needs to be AT LEAST 20 characters long)" 
-    read -s TF_VAR_master_password
-    if [[ ${#TF_VAR_master_password} -le 19 ]]; then
-        echo "**ERROR: MASTER PASSWORD must be AT LEAST 20 characters long"
-    else
-        echo "**INFO: MASTER PASSWORD saved, "${#TF_VAR_master_password}" characters long."
-        break
-    fi
-done
-
+#finish and run Terraform
+#########################
 
 echo "**INFO: Initialization finished. Ready to run with the following backend information (backend.tf):"
 cat backend.tf
